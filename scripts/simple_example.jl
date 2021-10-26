688d36146b81f42e879378c9ed8f0a00755ba2d2
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-10-26 16:35:46 +1100

    add modelling of co- and counter-rotating fields

--- scripts/simple_example.jl
+++ scripts/simple_example.jl
@@ -109,7 +109,10 @@ k_vals = exp.(log.(kmin):log(filterspacing):log(kmin)+(n_k_pts-1)*log(filterspac
 #near field
 zgrid = 0:0.01*R:1.5*R
 
-integration_kernel = [k * Hz_wavenumber.(k, z) for k in k_vals, z in zgrid]
+kcut(r) = pi/((filterspacing-1)*r)
+lowpass(k) = 1/(1 + (k/kcut(R))^3)
+
+integration_kernel = [lowpass(k) * k * Hz_wavenumber.(k, z) for k in k_vals, z in zgrid]
 
 #radial grid - number of r vals is equal to number of k vals minus base length of the filter + 1.
 #the base radius is rmax
@@ -474,3 +477,41 @@ xlabel("r (m)")
 ylabel("z (m)")
 
 display(gcf())
+##
+
+# find components of mag. field perpendicular
+# to earth's field, given inclination φ
+# and azimuthal coordinate θ
+function perpendicular_field(Bz, Br, φ, θ)
+    #y is defined as magnetic west.
+    #x is defined as
+    #cross product of y and the unit earth field vector
+    #(perpendicular to the earth field in the north-down plane)
+    Bperp_x = Bz * cos(φ) + Br * cos(θ) * sin(φ)
+    Bperp_y = Br * sin(θ) 
+    [Bperp_x; Bperp_y]
+end
+
+# find co-rotating and counter-rotating field parameters
+# need |B+|, |B-| and the phase lag ζt
+
+function co_counter_field(Bz, Br, φ, θ)
+    Bperp = perpendicular_field(Bz, Br, φ, θ)
+    Bstar = conj(Bperp)
+    BdotB = transpose(Bperp) * Bperp
+    BdotBstar =  transpose(Bperp) * Bstar
+    BcrossBstar =  real(im * (Bperp[1] * Bstar[2] - Bperp[2] * Bstar[1]))
+
+    exp_i_ζ = sqrt(BdotB/abs(BdotB))
+    αt = sqrt((BdotBstar + abs(BdotB))/2)
+    βt = sqrt((BdotBstar - abs(BdotB))/2)
+    
+    if BcrossBstar < 0
+        βt *= -1
+    end
+    
+    Bplus = αt - βt
+    Bminus = αt + βt
+
+    [Bplus; Bminus; exp_i_ζ]
+end
