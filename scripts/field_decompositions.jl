04b055bf6fc91e857574c1079ecf9e15d24d82f2
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-11-05 18:55:31 +1100

    more plotting bits for corotating and counter-rotating fields

--- scripts/field_decompositions.jl
+++ scripts/field_decompositions.jl
@@ -1,52 +1,77 @@
 using Revise, SNMRForward
 
-R = 10
-rgrid = 0.1:0.2:1.5*R
-zgrid = 0:0.1:1.5*R
+R = 50
+rgrid = 0.1:0.5:2*R
+zgrid = 0:0.5:2*R
 
 ## conductive half-space, at 2 kHz
-ωl = 2.0e3 #Hz, typical for Earth's field strength
+ωl = 2.5e3 #Hz, typical for Earth's field strength
 d = [Inf]
-σ = [0]
+σ = [0.05]
 
 #define k grid for j0 and j1 kernels
-        
-k_vals = reduce(hcat, SNMRForward.Filter_base/r for r in rgrid)
-k_vals_j1 = SNMRForward.Filter_base / R
+
+#j0        
+kj0 = reduce(hcat, SNMRForward.Filter_base/r for r in rgrid)
+rj0 = rgrid' .* ones(length(SNMRForward.Filter_base), length(rgrid))
+#j1
+kj1 = SNMRForward.Filter_base / R
 
 ## determine whether to use j0 or j1 for hz at each r
 use_j1 = [r < R for r in rgrid]
+j1_inds = cumsum(use_j1)
 
 ## compute layer responses to propagate fields
-B_halfspace, α_halfspace = SNMRForward.responses(σ, d, k_vals[:], ωl)
-B_halfspace_j1, α_halfspace_j1 = SNMRForward.responses(σ, d, k_vals_j1, ωl)
+Bj0, αj0 = SNMRForward.responses(σ, d, kj0[:], ωl)
+Bj1, αj1 = SNMRForward.responses(σ, d, kj1, ωl)
 
-## 
+## propagation coefficients for phi and phi' (Hz and Hr)
+phicj0, phipj0 = SNMRForward.phi_coeffs(Bj0, αj0, d, zgrid)
+phicj1, phipj1 = SNMRForward.phi_coeffs(Bj1, αj1, d, zgrid)
+## calculate z = 0 for j0 and j1 kernels
 
-phif_j0 = SNMRForward.phi_free.(k_vals[:], 0, R)
-phif_j1 = reduce(hcat, SNMRForward.phi_free_j1k.(k_vals_j1, 0, R, r) for r in rgrid if r < R)
+phif_j0 = SNMRForward.phi_free.(kj0[:], 0, R)
 
-phi0_j0 = 2 * k_vals[:]./(k_vals[:] .+ B_halfspace[:,1]) .* phif_j0
+phi0_j0 = 2 * kj0[:]./(kj0[:] .+ Bj0) .* phif_j0
 
-phi0_j1 = 2 * k_vals_j1./(k_vals_j1 .+ B_halfspace_j1[:,1]) .* phif_j1
+##
+rj1 = rgrid[use_j1]
+##
+phif_j1 = reduce(hcat, SNMRForward.phi_free_j1k.(kj1, 0, R, r) for r in rj1)
 
+phi0_j1 = 2 * kj1 ./ (kj1 .+ Bj1) .* phif_j1
 
 ## propagate through halfspace
-phiz_j0, phipz_j0 = SNMRForward.phiz(phi0_j0, B_halfspace, α_halfspace, d, zgrid)
+phiz_j0 = phi0_j0 .* phicj0
+phipz_j0 = phi0_j0 .* phipj0
 
-phiz_j1, _ = SNMRForward.phiz(phi0_j1, B_halfspace_j1, α_halfspace_j1, d, zgrid)
+##
+phiz_j1 = cat([phi0_j1[:,ir] .* phicj1 for ir=1:size(phi0_j1,2)]...; dims=3)
+
+##low-pass filters
+filterspacing = SNMRForward.Filter_base[2] / SNMRForward.Filter_base[1]
+kcut(r) = pi/((filterspacing-1)*r)
+lowpass_k(k,r) = 1/(1 + (k/kcut(r))^3)
+
+Hz_kernel_j1 =  lowpass_k.(kj1,R) .* kj1 .^ 3 .* permutedims(phiz_j1, (1,3,2))
+
 
 ## compute Hz and Hr field kernels for each k value
-Hz_kernel = reshape(k_vals[:].^3 .* phiz_j0, size(k_vals)..., length(zgrid))
+Hz_kernel = reshape(kj0[:].^3 .* phiz_j0, size(kj0)..., length(zgrid))
 # note the Hr kernel is actually in j1 space
 # due to the hankel transform property used to derive it
-Hr_kernel = reshape(- k_vals[:].^2 .* phipz_j0, size(k_vals)..., length(zgrid))
+Hr_kernel = reshape(-kj0[:].^2 .* phipz_j0, size(kj0)..., length(zgrid))
 
 # recover z and r fields on z and r grid
 Hz = zeros(ComplexF64, length(rgrid), length(zgrid))
 Hr = zeros(ComplexF64, length(rgrid), length(zgrid))
 for ir in 1:length(rgrid), iz in 1:length(zgrid)
-    Hz[ir, iz] = 1/rgrid[ir] * SNMRForward.Filter_J0' * Hz_kernel[:,ir,iz]
+    if use_j1[ir]
+        ir1 = j1_inds[ir]
+        Hz[ir,iz] = 1/R * SNMRForward.Filter_J1' * Hz_kernel_j1[:,ir1,iz]
+    else
+        Hz[ir, iz] = 1/rgrid[ir] * SNMRForward.Filter_J0' * Hz_kernel[:,ir,iz]
+    end
     Hr[ir, iz] = 1/rgrid[ir] * SNMRForward.Filter_J1' * Hr_kernel[:,ir,iz]
 end
 
@@ -62,29 +87,40 @@ gca().invert_yaxis()
 display(gcf())
 close(gcf())
 ## say the mag. field inclination is 60 degrees. calculate corotating part and phase lag
-Hfield_params = SNMRForward.co_counter_field.(Hz, Hr, 0, 0)
+Hfield_params = SNMRForward.co_counter_field.(Hz, Hr, 13*π/36, π/2)
 
 Hco = first.(Hfield_params)
 ζ = real.(last.(Hfield_params))
 
+
+H_counter = reshape([a[2] for a in Hfield_params[:]], size(Hco)...)
+
 ##
 fig, ax = subplots(1,2)
 sca(ax[1])
-pcolor(rgrid, zgrid, real.(Hco)', vmin = 0, vmax=0.06)
+pcolor(rgrid, zgrid, log.(real.(Hco))', vmin = -7.5, vmax=-2)
 gca().invert_yaxis()
 display(gcf())
 sca(ax[2])
-pcolor(rgrid, zgrid, ζ', vmin = -π/2, vmax=π/2)
+pcolor(rgrid, zgrid, log.(real.(H_counter))', vmin = -7.5, vmax=-2)
 gca().invert_yaxis()
 display(gcf())
 close(gcf())
 
 ##
-minimum(imag.(Hco))
+
+figure()
+pcolor(rgrid, zgrid, ζ', vmin = -π/2, vmax=π/2)
+gca().invert_yaxis()
+display(gcf())
+##
+maximum(imag.(Hco))
+
+
 
 ## get the full perpendicular field
-Hperp = SNMRForward.perpendicular_field.(Hz,Hr,0,0)
-Hperp_z = real.(first.(Hperp))
+Hperp = SNMRForward.perpendicular_field.(Hz,Hr,13*π/36,π/2)
+Hperp_z = imag.(first.(Hperp))
 
 figure()
 pcolor(rgrid, zgrid, Hperp_z', vmin = -0.06, vmax=0.06)
--- src/LayeredFields.jl
+++ src/LayeredFields.jl
@@ -57,3 +57,47 @@ function phiz(phi0, Bresponse, α, d, zgrid)
 
     phiz, phipz
 end
+
+#propagation coefficients in a layered earth
+#multiply these by field values at z = 0
+function phi_coeffs(Bresponse, α, d, zgrid)
+    #schelkunoff potential at the top of each layer
+    phi_tops = zeros(ComplexF64, size(Bresponse)...)
+    phi_tops[:,1] .= 1
+    for m=1:size(phi_tops,2)-1
+        phi_tops[:,m+1] = phi_tops[:,m] .* (α[:,m] + Bresponse[:,m])./(α[:,m] + Bresponse[:,m+1]) .* exp.(-α[:,m]*d[m])
+    end
+    #convert depth to z-values for the bottom of each layer
+    z_interface = cumsum(d)
+
+    phiz = zeros(ComplexF64, size(Bresponse,1), length(zgrid))
+    phipz = zeros(ComplexF64, size(Bresponse,1), length(zgrid))
+
+    layer_m = 1
+    coeff_minus = phi_tops[:,1] .* (1 .+ Bresponse[:,1]./α[:,1])/2
+    coeff_plus = 0
+    if length(d) > 1
+        coeff_plus = phi_tops[:,2] .* (1 .- Bresponse[:,2]./α[:,2])/2
+    end
+    h_m = 0
+    h_mp1 = z_interface[1]
+
+    for (iz, z) = enumerate(zgrid)
+        if z > h_mp1
+            coeff_minus = phi_tops[:,layer_m] .* (1 .+ Bresponse[:,layer_m]./α[:,layer_m])/2
+            coeff_plus = 0
+            if length(d) > layer_m
+                coeff_plus = phi_tops[:,layer_m+1] .* (1 .- Bresponse[:,layer_m+1]./α[:,layer_m+1])/2
+            end
+            h_m = h_mp1
+            h_mp1 = z_interface[layer_m]
+        end
+        
+        phiz[:,iz] = coeff_minus .* exp.(-α[:,layer_m]*(z-h_m)) +
+                        coeff_plus .* exp.(α[:,layer_m]*(z-h_mp1))
+        phipz[:,iz] = α[:,layer_m].* (coeff_plus .* exp.(α[:,layer_m]*(z-h_mp1))
+                        - coeff_minus .* exp.(-α[:,layer_m]*(z-h_m)))
+    end
+
+    phiz, phipz
+end
